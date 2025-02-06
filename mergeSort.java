7369b83d4a4da3fd0c16c135b273c601a9785dc3
gfradet <gf@fradet.net>
2025-02-06 17:53:29 -0500

    Update mergeSort.java

--- mergeSort.java
+++ mergeSort.java
@@ -2,8 +2,9 @@
 public static void sort(int[]arr){
 
       int[] arr2 = new int[arr.length];
+ int bar[] = {27,8,19,12,4,1,15,9}
+     printArray (bar);
 
 }
 //create an array
- int arr[] = {27,8,19,12,4,1,15,9}
-     printArray (arr);
+      
