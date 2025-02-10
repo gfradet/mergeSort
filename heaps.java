113a113bb42b142be416cccd9af396e78c423b0e
gfradet <gf@fradet.net>
2025-02-10 09:39:04 -0500

    Create heaps.java

---
+++ heaps.java
@@ -0,0 +1,21 @@
+public class Heap {
+
+//the actual storage structure for your heap
+
+private int[] arr;
+
+ 
+
+//constructor for your heap
+
+//feel free to make one that takes in an array if you prefer for your testing purposes.
+
+//note that we will not be inserting more than 100 elements into our array so you need not worry about re-sizing
+
+//the array
+
+public Heap() {
+
+arr = new int[100];
+
+}
